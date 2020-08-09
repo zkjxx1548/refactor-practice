e5c7d4375063eec9630bc2db5bbc565f57d7b5a7
zkj <438101845@qq.com>
2020-08-09 23:40:51 +0800

    [zkj]-{refactor}:[6] code smell - Long function

--- 6/src/main/java/com/twu/refactoring/Customer.java
+++ 6/src/main/java/com/twu/refactoring/Customer.java
@@ -21,30 +21,10 @@ public class Customer {
 	}
 
 	public String statement() {
-		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		double totalAmount = getTotalAmount(rentalList);
-
-
-		result += rentalList.stream()
-				.map(rental -> {
-					Movietype movietype = rental.getMovie().returnTypeMovie(rental.getMovie().getPriceCode());
-					movietype.setMovieTitle(rental.getMovie().getTitle());
-					movietype.setDaysRented(rental.getDaysRented());
-					return movietype;
-				})
-				.map(movietype1 -> "\t" + movietype1.getMovieTitle() + "\t"
-						+ movietype1.getAmount(movietype1.getDaysRented()) + "\n")
-				.reduce(String::concat)
-				.orElse("");
-
-		frequentRenterPoints += rentalList.size();
-		frequentRenterPoints += rentalList.stream()
-				.filter(rental -> {
-					Movietype movietype = rental.getMovie().returnTypeMovie(rental.getMovie().getPriceCode());
-					return movietype instanceof NewReleaseMovie && rental.getDaysRented() > 1;
-				})
-				.count();
+		result += getResultOfRent(rentalList);
+		int frequentRenterPoints = getFrequentRenterPoints(rentalList);
 
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
@@ -62,4 +42,27 @@ public class Customer {
 				.reduce(Double::sum).orElse(0d);
 	}
 
+	public String getResultOfRent(List<Rental> rentalList) {
+		return rentalList.stream()
+				.map(rental -> {
+					Movietype movietype = rental.getMovie().returnTypeMovie(rental.getMovie().getPriceCode());
+					movietype.setMovieTitle(rental.getMovie().getTitle());
+					movietype.setDaysRented(rental.getDaysRented());
+					return movietype;
+				})
+				.map(movietype -> "\t" + movietype.getMovieTitle() + "\t"
+						+ movietype.getAmount(movietype.getDaysRented()) + "\n")
+				.reduce(String::concat)
+				.orElse("");
+	}
+
+	public int getFrequentRenterPoints(List<Rental> rentalList) {
+		long count = rentalList.stream()
+				.filter(rental -> {
+					Movietype movietype = rental.getMovie().returnTypeMovie(rental.getMovie().getPriceCode());
+					return movietype instanceof NewReleaseMovie && rental.getDaysRented() > 1;
+				})
+				.count();
+		return rentalList.size() + (int) count;
+	}
 }
