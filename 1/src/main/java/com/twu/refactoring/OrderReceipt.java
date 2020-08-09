b9fd44d5ba354cbfd7d0ea212ee21015e76a2da6
zkj <438101845@qq.com>
2020-08-09 18:37:48 +0800

    [zkj]-{refactor}: code smell - Comments

--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -16,17 +16,9 @@ public class OrderReceipt {
 
 	public String printReceipt() {
 		StringBuilder output = new StringBuilder();
-
-		// print headers
 		output.append("======Printing Orders======\n");
-
-		// print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
-
-		// prints lineItems
 		double totSalesTx = 0d;
 		double tot = 0d;
 		for (LineItem lineItem : order.getLineItems()) {
@@ -38,19 +30,11 @@ public class OrderReceipt {
 			output.append('\t');
 			output.append(lineItem.totalAmount());
 			output.append('\n');
-
-			// calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
             totSalesTx += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += lineItem.totalAmount() + salesTax;
 		}
-
-		// prints the state tax
 		output.append("Sales Tax").append('\t').append(totSalesTx);
-
-        // print total amount
 		output.append("Total Amount").append('\t').append(tot);
 		return output.toString();
 	}
